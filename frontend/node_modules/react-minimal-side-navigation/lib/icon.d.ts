573a22735d4223664d22ba2ae4f8b1ca884ec43d
Vladik Romanovsky <vromanso@redhat.com>
2022-11-16 20:39:14 -0500

    stop tracking frontend/node_modules
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- frontend/node_modules/react-minimal-side-navigation/lib/icon.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-declare type IconProps = {
-    size?: string | number;
-    stroke?: string;
-};
-export declare const ChevronDownIcon: React.NamedExoticComponent<IconProps>;
-export declare const ChevronUpIcon: React.NamedExoticComponent<IconProps>;
-export {};
--- frontend/node_modules/react-minimal-side-navigation/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as Navigation } from './side-nav';
-export type { NavItemProps, SideNavProps } from './side-nav';
