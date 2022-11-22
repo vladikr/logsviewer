e2df22bb729814717b1809e0f5043945b4b14482
Vladik Romanovsky <vromanso@redhat.com>
2022-11-22 11:33:51 -0500

    frontend: enable VMIs table
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- frontend/src/pages/vmis.jsx
+++ frontend/src/pages/vmis.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-
 import {DashboardLayout} from '../components/Layout';
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import {VmisTable} from './vmisTable';
 
+const queryClient = new QueryClient();
 const VMIsPage = () => {
   return (
     <DashboardLayout>
-      <h2>VMIs Page</h2>
+      <React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+		  <VmisTable />
+		</QueryClientProvider>
+      </React.StrictMode>
     </DashboardLayout>
   )
 }
