e43327df130e8445991ed9f8331584d0d14e9836
João Vilaça <jvilaca@redhat.com>
2023-06-07 10:56:48 +0100

    Add CLI tool to spin up logsviewer and import must-gather
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

---
+++ tools/lvctl/pkg/lvctl/lvctl.go
@@ -0,0 +1,143 @@
+package lvctl
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	routeClient "github.com/openshift/client-go/route/clientset/versioned"
+	templateClient "github.com/openshift/client-go/template/clientset/versioned"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/klog/v2"
+)
+
+const (
+	defaultLogsviewerImage = "quay.io/vladikr/logsviewer:devel"
+	defaultStorageClass    = "ocs-storagecluster-ceph-rbd"
+)
+
+type LogsViewer struct {
+	instanceID         string
+	namespace          string
+	storageClass       string
+	image              string
+	mustGatherFileName string
+
+	kubeconfig     string
+	k8sClient      *kubernetes.Clientset
+	templateClient *templateClient.Clientset
+	routeClient    *routeClient.Clientset
+}
+
+func Run() {
+	lg := &LogsViewer{}
+
+	setupCommand, deleteCommand, importCommand, setupImportCommand := lg.setupFlags()
+
+	if len(os.Args) < 2 {
+		klog.Exit("expected 'setup', 'delete', 'import' or 'setupImportCommand' subcommands")
+	}
+
+	lg.createClientset()
+
+	switch os.Args[1] {
+	case "help":
+		fmt.Println("Control an instance of the logsViewer")
+		fmt.Println("Syntax: lvctl [command] [options]")
+		setupCommand.Usage()
+		deleteCommand.Usage()
+		importCommand.Usage()
+		setupImportCommand.Usage()
+	case "setup":
+		mustSucceed(setupCommand.Parse(os.Args[2:]))
+		lg.setup()
+	case "delete":
+		mustSucceed(deleteCommand.Parse(os.Args[2:]))
+		lg.delete()
+	case "import":
+		mustSucceed(importCommand.Parse(os.Args[2:]))
+		lg.importMustGather()
+	case "setup-import":
+		mustSucceed(setupImportCommand.Parse(os.Args[2:]))
+		lg.setup()
+		lg.importMustGather()
+	default:
+		klog.Exit("unknown command: ", os.Args[1])
+	}
+}
+
+func (lg *LogsViewer) setupFlags() (setupCommand, deleteCommand, importCommand, setupImportCommand *flag.FlagSet) {
+	setupCommand = flag.NewFlagSet("setup", flag.ExitOnError)
+	lg.commonFlags(setupCommand)
+	setupCommand.StringVar(&lg.storageClass, "storage-class", defaultStorageClass, "The storage class to use")
+	setupCommand.StringVar(&lg.image, "image", defaultLogsviewerImage, "The LogsViewer image to use")
+
+	deleteCommand = flag.NewFlagSet("delete", flag.ExitOnError)
+	lg.commonFlags(deleteCommand)
+
+	importCommand = flag.NewFlagSet("import", flag.ExitOnError)
+	lg.commonFlags(importCommand)
+	importCommand.StringVar(&lg.mustGatherFileName, "file", "", "The must-gather file to import")
+
+	setupImportCommand = flag.NewFlagSet("setup-import", flag.ExitOnError)
+	lg.commonFlags(setupImportCommand)
+	setupImportCommand.StringVar(&lg.storageClass, "storage-class", defaultStorageClass, "The storage class to use")
+	setupImportCommand.StringVar(&lg.image, "image", defaultLogsviewerImage, "The LogsViewer image to use")
+	setupImportCommand.StringVar(&lg.mustGatherFileName, "file", "", "The must-gather file to import")
+
+	return
+}
+
+func (lg *LogsViewer) commonFlags(flagSet *flag.FlagSet) {
+	flagSet.StringVar(&lg.instanceID, "id", "", "The instance id")
+	flagSet.StringVar(&lg.namespace, "namespace", "", "The namespace (default is the current namespace)")
+
+	if home := homedir.HomeDir(); home != "" {
+		flagSet.StringVar(&lg.kubeconfig, "kubeconfig", filepath.Join(home, ".kube", "config"), "absolute path to the kubeconfig file")
+	} else {
+		flagSet.StringVar(&lg.kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+}
+
+func (lg *LogsViewer) createClientset() {
+	config := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: lg.kubeconfig},
+		&clientcmd.ConfigOverrides{},
+	)
+	rawConfig, err := config.RawConfig()
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+	if lg.namespace == "" {
+		lg.namespace = rawConfig.Contexts[rawConfig.CurrentContext].Namespace
+	}
+
+	clientConfig, err := config.ClientConfig()
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+
+	lg.k8sClient, err = kubernetes.NewForConfig(clientConfig)
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+
+	lg.templateClient, err = templateClient.NewForConfig(clientConfig)
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+
+	lg.routeClient, err = routeClient.NewForConfig(clientConfig)
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+}
+
+func mustSucceed(err error) {
+	if err != nil {
+		klog.Exit(err.Error())
+	}
+}
---
+++ tools/lvctl/pkg/lvctl/setup.go
@@ -0,0 +1,204 @@
+package lvctl
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	templatev1 "github.com/openshift/api/template/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/yaml"
+)
+
+func (lg *LogsViewer) setup() {
+	lg.generateInstanceID()
+	klog.Infof("setting up LogsViewer %s", lg.instanceID)
+
+	template, err := loadTemplate("../../deployment/elk_pod_template.yaml")
+	if err != nil {
+		klog.Exit("failed to load template: ", err)
+	}
+
+	template, err = lg.createTemplateInstance(template)
+	if err != nil {
+		klog.Exit("failed to create template: ", err)
+	}
+
+	err = lg.instantiateTemplate(template)
+	if err != nil {
+		klog.Exit("failed to instantiate template: ", err)
+	}
+
+	err = lg.waitTemplateInstanceToBeReady()
+	if err != nil {
+		klog.Exit("failed to wait for template instance to be ready: ", err)
+	}
+
+	klog.Info("waiting for LogsViewer pod to be ready...")
+	err = lg.waitForPodToBeReady()
+	if err != nil {
+		klog.Exit("failed to wait for pod to be ready: ", err)
+	}
+
+	route, err := lg.routeClient.RouteV1().Routes(lg.namespace).Get(context.TODO(), "logsviewer-"+lg.instanceID, metav1.GetOptions{})
+	if err != nil {
+		klog.Exit("failed to get route: ", err)
+	}
+
+	klog.Infof("logsViewer %s is available at %s", lg.instanceID, route.Status.Ingress[0].Host)
+	klog.Infof("logsViewer %s setup complete", lg.instanceID)
+}
+
+func (lg *LogsViewer) generateInstanceID() {
+	if lg.instanceID != "" {
+		return
+	}
+
+	lg.instanceID = rand.String(6)
+}
+
+func (lg *LogsViewer) createTemplateInstance(template *templatev1.Template) (*templatev1.Template, error) {
+	template.Name = template.Name + "-" + lg.instanceID
+
+	template.Parameters = []templatev1.Parameter{
+		{
+			Name:  "SUFFIX",
+			Value: lg.instanceID,
+		},
+		{
+			Name:  "STORAGE_CLASS",
+			Value: lg.storageClass,
+		},
+		{
+			Name:  "LOGSVIEWER_IMAGE",
+			Value: lg.image,
+		},
+	}
+
+	template, err := lg.templateClient.TemplateV1().Templates(lg.namespace).Create(context.TODO(), template, metav1.CreateOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	klog.Infof("template '%s' created", template.Name)
+
+	return template, nil
+}
+
+func (lg *LogsViewer) instantiateTemplate(template *templatev1.Template) error {
+	ti, err := lg.templateClient.TemplateV1().TemplateInstances(lg.namespace).Create(
+		context.TODO(),
+		&templatev1.TemplateInstance{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "logsviewer-" + lg.instanceID,
+			},
+			Spec: templatev1.TemplateInstanceSpec{
+				Template: *template,
+			},
+		},
+		metav1.CreateOptions{},
+	)
+	if err != nil {
+		return err
+	}
+
+	klog.Infof("template Instance '%s' created", ti.Name)
+
+	return nil
+}
+
+func (lg *LogsViewer) waitTemplateInstanceToBeReady() error {
+	klog.Info("waiting for Template Instance to be ready...")
+
+	watcher, err := lg.templateClient.TemplateV1().TemplateInstances(lg.namespace).Watch(context.TODO(), metav1.ListOptions{
+		FieldSelector: "metadata.name=logsviewer-" + lg.instanceID,
+	})
+	if err != nil {
+		return err
+	}
+
+	for event := range watcher.ResultChan() {
+		ti, ok := event.Object.(*templatev1.TemplateInstance)
+		if !ok {
+			klog.Warning("unexpected object type")
+			continue
+		}
+
+		for _, condition := range ti.Status.Conditions {
+			if condition.Type == templatev1.TemplateInstanceReady {
+				if condition.Status == corev1.ConditionTrue {
+					klog.Info("template Instance is ready")
+					return nil
+				}
+			}
+
+			if condition.Type == templatev1.TemplateInstanceInstantiateFailure {
+				if condition.Status == corev1.ConditionTrue {
+					return fmt.Errorf("template Instance failed: %s", condition.Message)
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func (lg *LogsViewer) waitForPodToBeReady() error {
+	watcher, err := lg.k8sClient.CoreV1().Pods(lg.namespace).Watch(context.TODO(), metav1.ListOptions{
+		FieldSelector: "metadata.name=logsviewer-" + lg.instanceID,
+	})
+	if err != nil {
+		return err
+	}
+
+	var phase corev1.PodPhase
+	for event := range watcher.ResultChan() {
+		pod, ok := event.Object.(*corev1.Pod)
+		if !ok {
+			klog.Warning("unexpected object type")
+			continue
+		}
+
+		if pod.Status.Phase == corev1.PodRunning {
+			if !logsViewerContainerReady(pod) {
+				klog.Info("logsViewer container is not ready yet")
+			} else {
+				klog.Info("logsViewer is ready")
+				break
+			}
+		} else if pod.Status.Phase != phase {
+			klog.Infof("logsViewer pod is not ready yet, current phase: %s", pod.Status.Phase)
+			phase = pod.Status.Phase
+		}
+	}
+
+	return nil
+}
+
+func logsViewerContainerReady(pod *corev1.Pod) bool {
+	for _, containerStatus := range pod.Status.ContainerStatuses {
+		if containerStatus.Name == "logsviewer" {
+			return containerStatus.Ready
+		}
+	}
+
+	return false
+}
+
+func loadTemplate(templateFile string) (*templatev1.Template, error) {
+	tpl, err := os.ReadFile(templateFile)
+	if err != nil {
+		return nil, err
+	}
+
+	template := &templatev1.Template{}
+	err = yaml.Unmarshal(tpl, template)
+	if err != nil {
+		return nil, err
+	}
+
+	return template, nil
+}
