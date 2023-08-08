5627d2f57b5c51bc38147f9a46b3077e52276914
João Vilaça <jvilaca@redhat.com>
2023-08-08 11:06:36 +0100

    Save subscription object
    Signed-off-by: João Vilaça <jvilaca@redhat.com>

--- go.mod
+++ go.mod
@@ -8,27 +8,30 @@ require (
 	github.com/machadovilaca/operator-observability v0.0.2
 	github.com/openshift/api v0.0.0-20211217221424-8779abfbd571
 	github.com/openshift/client-go v0.0.0-00010101000000-000000000000
-	github.com/prometheus/client_golang v1.14.0
-	github.com/prometheus/client_model v0.3.0
+	github.com/operator-framework/api v0.17.7
+	github.com/prometheus/client_golang v1.15.1
+	github.com/prometheus/client_model v0.4.0
 	github.com/rs/cors v1.9.0
 	gopkg.in/yaml.v3 v3.0.1
-	k8s.io/api v0.26.1
-	k8s.io/apimachinery v0.26.1
-	k8s.io/client-go v0.26.1
+	k8s.io/api v0.27.2
+	k8s.io/apimachinery v0.27.2
+	k8s.io/client-go v0.27.2
 	kubevirt.io/api v0.59.0
 	sigs.k8s.io/yaml v1.3.0
 )
 
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
-	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.1 // indirect
 	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
@@ -40,28 +43,29 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.2 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/openshift/custom-resource-status v1.1.2 // indirect
 	github.com/pborman/uuid v1.2.0 // indirect
 	github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring v0.64.1 // indirect
-	github.com/prometheus/common v0.37.0 // indirect
-	github.com/prometheus/procfs v0.8.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.0.0-20220223155221-ee480838109b // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/sirupsen/logrus v1.9.2 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.5.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.1 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/apiextensions-apiserver v0.26.1 // indirect
+	k8s.io/apiextensions-apiserver v0.27.2 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
-	k8s.io/kube-openapi v0.0.0-20230308215209-15aac26d736a // indirect
+	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
 	kubevirt.io/containerized-data-importer-api v1.55.0 // indirect
 	kubevirt.io/controller-lifecycle-operator-sdk/api v0.0.0-20220329064328-f3cc58c6ed90 // indirect
--- pkg/backend/db/observer.go
+++ pkg/backend/db/observer.go
@@ -2,9 +2,10 @@ package db
 
 import (
 	"fmt"
+	"github.com/operator-framework/api/pkg/operators/v1alpha1"
+	"strings"
 	"sync"
 	"time"
-    "strings"
 
 	k8sv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/json"
@@ -12,33 +13,32 @@ import (
 	"k8s.io/client-go/util/workqueue"
 
 	kubevirtv1 "kubevirt.io/api/core/v1"
-    
-    "logsviewer/pkg/backend/log"
+
+	"logsviewer/pkg/backend/log"
 )
 
 func NewObjectStore(storeDB *DatabaseInstance) *ObjectStore {
 
 	queue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "objectStore")
 	c := &ObjectStore{
-		Queue:             queue,
-		lockDBConn:        &sync.Mutex{},
-        storeDB:           storeDB,
+		Queue:      queue,
+		lockDBConn: &sync.Mutex{},
+		storeDB:    storeDB,
 	}
 
 	return c
 }
 
 type ObjectStore struct {
-	Queue             workqueue.RateLimitingInterface
-	storeDB           *DatabaseInstance
-	lockDBConn   	  *sync.Mutex
-    wg                sync.WaitGroup
-
+	Queue      workqueue.RateLimitingInterface
+	storeDB    *DatabaseInstance
+	lockDBConn *sync.Mutex
+	wg         sync.WaitGroup
 }
 
 func (c *ObjectStore) Run(threadiness int, stopCh chan struct{}) {
 	defer c.Queue.ShutDown()
-    //workers := 1
+	//workers := 1
 
 	// Start the actual work
 	for i := 0; i < threadiness; i++ {
@@ -46,12 +46,12 @@ func (c *ObjectStore) Run(threadiness int, stopCh chan struct{}) {
 	}
 
 	<-stopCh
-    c.wg.Wait()
-    
+	c.wg.Wait()
+
 }
 
 func (c *ObjectStore) runWorker() {
-    log.Log.Println("runWorker")
+	log.Log.Println("runWorker")
 	for c.Execute() {
 	}
 }
@@ -63,7 +63,7 @@ func (c *ObjectStore) connectDatabaseIfNeeded() bool {
 
 		dbInst, err := NewDatabaseInstance()
 		if err != nil {
-            log.Log.Println("failed to connect to database", err)
+			log.Log.Println("failed to connect to database", err)
 			return false
 		}
 		c.storeDB = dbInst
@@ -74,7 +74,7 @@ func (c *ObjectStore) connectDatabaseIfNeeded() bool {
 
 func (c *ObjectStore) Execute() bool {
 	if succedded := c.connectDatabaseIfNeeded(); !succedded {
-        log.Log.Println("failed to connect to DB")
+		log.Log.Println("failed to connect to DB")
 		return false
 	}
 
@@ -84,10 +84,10 @@ func (c *ObjectStore) Execute() bool {
 	}
 	defer c.Queue.Done(obj)
 	if err := c.execute(obj); err != nil {
-        log.Log.Println("re-enqueuing object ", obj, " err: ", err)
+		log.Log.Println("re-enqueuing object ", obj, " err: ", err)
 	} else {
 		c.Queue.Forget(obj)
-        c.wg.Done()
+		c.wg.Done()
 	}
 
 	return true
@@ -103,265 +103,297 @@ func (d *ObjectStore) execute(obj interface{}) error {
 }
 
 func (d *ObjectStore) countPodContainers(pod *k8sv1.Pod) (int, int) {
-    totalContainers := len(pod.Spec.Containers)
-    activeContainers := 0
-    for _, container := range pod.Status.ContainerStatuses {
-        if container.State.Running != nil {
-            activeContainers++
-        }
-    }
-    return totalContainers, activeContainers
+	totalContainers := len(pod.Spec.Containers)
+	activeContainers := 0
+	for _, container := range pod.Status.ContainerStatuses {
+		if container.State.Running != nil {
+			activeContainers++
+		}
+	}
+	return totalContainers, activeContainers
 }
 
 func (d *ObjectStore) formatPodPVCs(pod *k8sv1.Pod) string {
-    pvcs := []string{}
+	pvcs := []string{}
 
-    for _, volume := range pod.Spec.Volumes {
-        if volume.PersistentVolumeClaim != nil {
-            pvcs = append(pvcs, volume.PersistentVolumeClaim.ClaimName)
-        }
-    }
-    pvcsStr := strings.Join(pvcs, ",")
-    return pvcsStr
+	for _, volume := range pod.Spec.Volumes {
+		if volume.PersistentVolumeClaim != nil {
+			pvcs = append(pvcs, volume.PersistentVolumeClaim.ClaimName)
+		}
+	}
+	pvcsStr := strings.Join(pvcs, ",")
+	return pvcsStr
 }
 
 func (d *ObjectStore) storePod(pod *k8sv1.Pod) error {
-    jsonBytes, err := json.Marshal(pod)
-    if err != nil {
-        log.Log.Println("failed to marshal pod object ", pod, " err: ", err)
-    }
+	jsonBytes, err := json.Marshal(pod)
+	if err != nil {
+		log.Log.Println("failed to marshal pod object ", pod, " err: ", err)
+	}
 
-    createdByUID := pod.Labels[kubevirtv1.CreatedByLabel]
-    totalContainers, activeContainers := d.countPodContainers(pod)
-    name := pod.GetObjectMeta().GetName()
-    namespace := pod.GetObjectMeta().GetNamespace()
-    uid := string(pod.GetObjectMeta().GetUID())
-    kind := pod.GetObjectKind().GroupVersionKind().Kind
-    pvcsList := d.formatPodPVCs(pod)
+	createdByUID := pod.Labels[kubevirtv1.CreatedByLabel]
+	totalContainers, activeContainers := d.countPodContainers(pod)
+	name := pod.GetObjectMeta().GetName()
+	namespace := pod.GetObjectMeta().GetNamespace()
+	uid := string(pod.GetObjectMeta().GetUID())
+	kind := pod.GetObjectKind().GroupVersionKind().Kind
+	pvcsList := d.formatPodPVCs(pod)
 	storeObj := &Pod{
-		Key:       fmt.Sprintf("%s/%s", name, namespace),
-		Kind:      kind,
-		Name:      name,
-		Namespace: namespace,
-		UUID:      uid,
-        Phase:     string(pod.Status.Phase),    
-        ActiveContainers: activeContainers,
-        TotalContainers: totalContainers,
-        NodeName: string(pod.Spec.NodeName),
-        CreationTime: pod.CreationTimestamp,
-        PVCs: pvcsList,
-        Content: jsonBytes,
-        CreatedBy: createdByUID,
+		Key:              fmt.Sprintf("%s/%s", name, namespace),
+		Kind:             kind,
+		Name:             name,
+		Namespace:        namespace,
+		UUID:             uid,
+		Phase:            string(pod.Status.Phase),
+		ActiveContainers: activeContainers,
+		TotalContainers:  totalContainers,
+		NodeName:         string(pod.Spec.NodeName),
+		CreationTime:     pod.CreationTimestamp,
+		PVCs:             pvcsList,
+		Content:          jsonBytes,
+		CreatedBy:        createdByUID,
 	}
 	if err := d.storeDB.StorePod(storeObj); err != nil {
-        log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
+		log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
 		return err
 	}
 	return nil
 }
 
 func (d *ObjectStore) storeNode(node *k8sv1.Node) error {
-    jsonBytes, err := json.Marshal(node)
-    if err != nil {
-        log.Log.Println("failed to marshal node object ", node, " err: ", err)
-    }
+	jsonBytes, err := json.Marshal(node)
+	if err != nil {
+		log.Log.Println("failed to marshal node object ", node, " err: ", err)
+	}
 
-    nodeStatus := "NotReady"
-    for _, condition := range node.Status.Conditions {
-        if condition.Type == "Ready" {
-            nodeStatus = "Ready"
-        }
-    }
+	nodeStatus := "NotReady"
+	for _, condition := range node.Status.Conditions {
+		if condition.Type == "Ready" {
+			nodeStatus = "Ready"
+		}
+	}
 
-    nodeInternalIP := ""
-    nodeHostName := ""
-    for _, address := range node.Status.Addresses {
-        switch address.Type {
-        case "InternalIP":
-            nodeInternalIP = address.Address
-        case "Hostname":
-            nodeHostName = address.Address
-        default:
-            continue
-        }
-    }
+	nodeInternalIP := ""
+	nodeHostName := ""
+	for _, address := range node.Status.Addresses {
+		switch address.Type {
+		case "InternalIP":
+			nodeInternalIP = address.Address
+		case "Hostname":
+			nodeHostName = address.Address
+		default:
+			continue
+		}
+	}
 
-    name := node.GetObjectMeta().GetName()
+	name := node.GetObjectMeta().GetName()
 	storeObj := &Node{
-		Name:      name,
-        Status:    nodeStatus,    
-		SystemUUID: node.Status.NodeInfo.SystemUUID,
-        InternalIP: nodeInternalIP,
-        HostName: nodeHostName,
-        OsImage: node.Status.NodeInfo.OSImage,
-        KernelVersion: node.Status.NodeInfo.KernelVersion,
-        KubletVersion: node.Status.NodeInfo.KubeletVersion,
-        ContainerRuntimeVersion: node.Status.NodeInfo.ContainerRuntimeVersion,
-        Content: jsonBytes,
+		Name:                    name,
+		Status:                  nodeStatus,
+		SystemUUID:              node.Status.NodeInfo.SystemUUID,
+		InternalIP:              nodeInternalIP,
+		HostName:                nodeHostName,
+		OsImage:                 node.Status.NodeInfo.OSImage,
+		KernelVersion:           node.Status.NodeInfo.KernelVersion,
+		KubletVersion:           node.Status.NodeInfo.KubeletVersion,
+		ContainerRuntimeVersion: node.Status.NodeInfo.ContainerRuntimeVersion,
+		Content:                 jsonBytes,
 	}
 	if err := d.storeDB.StoreNode(storeObj); err != nil {
-        log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
+		log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
 		return err
 	}
 	return nil
 }
 
-
 func (d *ObjectStore) storeVmi(vmi *kubevirtv1.VirtualMachineInstance) error {
-    jsonBytes, err := json.Marshal(vmi)
-    if err != nil {
-        log.Log.Println("failed to marshal vmi object ", vmi, " err: ", err)
-    }
+	jsonBytes, err := json.Marshal(vmi)
+	if err != nil {
+		log.Log.Println("failed to marshal vmi object ", vmi, " err: ", err)
+	}
 
-    name := vmi.GetObjectMeta().GetName()
-    namespace := vmi.GetObjectMeta().GetNamespace()
-    uid := string(vmi.GetObjectMeta().GetUID())
+	name := vmi.GetObjectMeta().GetName()
+	namespace := vmi.GetObjectMeta().GetNamespace()
+	uid := string(vmi.GetObjectMeta().GetUID())
 	storeObj := &VirtualMachineInstance{
-		Name:      name,
-		Namespace: namespace,
-		UUID:      uid,
-        Phase:     string(vmi.Status.Phase),    
-        Reason:     string(vmi.Status.Reason),    
-        NodeName: string(vmi.Status.NodeName),
-        CreationTime: vmi.CreationTimestamp,
-        Status: vmi.Status,
-        Content: jsonBytes,
+		Name:         name,
+		Namespace:    namespace,
+		UUID:         uid,
+		Phase:        string(vmi.Status.Phase),
+		Reason:       string(vmi.Status.Reason),
+		NodeName:     string(vmi.Status.NodeName),
+		CreationTime: vmi.CreationTimestamp,
+		Status:       vmi.Status,
+		Content:      jsonBytes,
 	}
 	if err := d.storeDB.StoreVmi(storeObj); err != nil {
-        log.Log.Println("failed to store vmi obj  ", storeObj, " err: ", err)
+		log.Log.Println("failed to store vmi obj  ", storeObj, " err: ", err)
 		return err
 	}
 	return nil
 }
 
 func (d *ObjectStore) storeVmiMigration(vmim kubevirtv1.VirtualMachineInstanceMigration) error {
-    jsonBytes, err := json.Marshal(vmim)
-    if err != nil {
-        log.Log.Println("failed to marshal vmi migration object ", vmim, " err: ", err)
-        return err
-    }
-
-    name := vmim.GetObjectMeta().GetName()
-    namespace := vmim.GetObjectMeta().GetNamespace()
-    uid := string(vmim.GetObjectMeta().GetUID())
-	storeObj := &VirtualMachineInstanceMigration{
-		Name:      name,
-		Namespace: namespace,
-		UUID:      uid,
-        Phase:     string(vmim.Status.Phase),    
-        VMIName: string(vmim.Spec.VMIName),
-        CreationTime: vmim.CreationTimestamp,
-        Content: jsonBytes,
+	jsonBytes, err := json.Marshal(vmim)
+	if err != nil {
+		log.Log.Println("failed to marshal vmi migration object ", vmim, " err: ", err)
+		return err
 	}
 
-    if migrationState := vmim.Status.MigrationState; migrationState != nil {
-        storeObj.TargetPod = migrationState.TargetPod
-        storeObj.CreationTime = *migrationState.StartTimestamp
-        storeObj.EndTimestamp = *migrationState.EndTimestamp
-        storeObj.SourceNode = migrationState.SourceNode
-        storeObj.TargetNode = migrationState.TargetNode
-        storeObj.Completed = migrationState.Completed
-        storeObj.Failed = migrationState.Failed
-    }
+	name := vmim.GetObjectMeta().GetName()
+	namespace := vmim.GetObjectMeta().GetNamespace()
+	uid := string(vmim.GetObjectMeta().GetUID())
+	storeObj := &VirtualMachineInstanceMigration{
+		Name:         name,
+		Namespace:    namespace,
+		UUID:         uid,
+		Phase:        string(vmim.Status.Phase),
+		VMIName:      string(vmim.Spec.VMIName),
+		CreationTime: vmim.CreationTimestamp,
+		Content:      jsonBytes,
+	}
+
+	if migrationState := vmim.Status.MigrationState; migrationState != nil {
+		storeObj.TargetPod = migrationState.TargetPod
+		storeObj.CreationTime = *migrationState.StartTimestamp
+		storeObj.EndTimestamp = *migrationState.EndTimestamp
+		storeObj.SourceNode = migrationState.SourceNode
+		storeObj.TargetNode = migrationState.TargetNode
+		storeObj.Completed = migrationState.Completed
+		storeObj.Failed = migrationState.Failed
+	}
 	if err := d.storeDB.StoreVmiMigration(storeObj); err != nil {
-        log.Log.Println("failed to store vmi migration obj  ", storeObj, " err: ", err)
+		log.Log.Println("failed to store vmi migration obj  ", storeObj, " err: ", err)
 		return err
 	}
 	return nil
 }
 
 func (d *ObjectStore) storePVC(pvc *k8sv1.PersistentVolumeClaim) error {
-    jsonBytes, err := json.Marshal(pvc)
-    if err != nil {
-        log.Log.Println("failed to marshal pvc object ", pvc, " err: ", err)
-    }
+	jsonBytes, err := json.Marshal(pvc)
+	if err != nil {
+		log.Log.Println("failed to marshal pvc object ", pvc, " err: ", err)
+	}
 
-    name := pvc.GetObjectMeta().GetName()
-    namespace := pvc.GetObjectMeta().GetNamespace()
-    uid := string(pvc.GetObjectMeta().GetUID())
-    storage := pvc.Status.Capacity.Storage()
-    capacity := storage.String()
+	name := pvc.GetObjectMeta().GetName()
+	namespace := pvc.GetObjectMeta().GetNamespace()
+	uid := string(pvc.GetObjectMeta().GetUID())
+	storage := pvc.Status.Capacity.Storage()
+	capacity := storage.String()
 
-    accessModes := ""
-    if len(pvc.Status.AccessModes) > 0 {
-        accessModes = string(pvc.Status.AccessModes[0])
-        for i := 1; i < len(pvc.Status.AccessModes); i++ {
-            accessModes += fmt.Sprintf(",%s", pvc.Status.AccessModes[i])
-        }
-    }
-        
-    storageClassName := ""
-    if pvc.Spec.StorageClassName != nil {
-        storageClassName = *pvc.Spec.StorageClassName
-    }
+	accessModes := ""
+	if len(pvc.Status.AccessModes) > 0 {
+		accessModes = string(pvc.Status.AccessModes[0])
+		for i := 1; i < len(pvc.Status.AccessModes); i++ {
+			accessModes += fmt.Sprintf(",%s", pvc.Status.AccessModes[i])
+		}
+	}
 
-    volumeMode := ""
-    if pvc.Spec.VolumeMode != nil {
-        volumeMode = string(*pvc.Spec.VolumeMode)
-    }
+	storageClassName := ""
+	if pvc.Spec.StorageClassName != nil {
+		storageClassName = *pvc.Spec.StorageClassName
+	}
+
+	volumeMode := ""
+	if pvc.Spec.VolumeMode != nil {
+		volumeMode = string(*pvc.Spec.VolumeMode)
+	}
 
 	storeObj := &PersistentVolumeClaim{
-		Name:      name,
-		Namespace: namespace,
-		UUID:      uid,
-        Phase:     string(pvc.Status.Phase),
-        AccessModes: accessModes,
-        StorageClassName: storageClassName,
-        VolumeName: pvc.Spec.VolumeName,
-        VolumeMode: volumeMode,
-        Reason: "",
-        Capacity: capacity,
-        CreationTime: pvc.CreationTimestamp,
-        Content: jsonBytes,
+		Name:             name,
+		Namespace:        namespace,
+		UUID:             uid,
+		Phase:            string(pvc.Status.Phase),
+		AccessModes:      accessModes,
+		StorageClassName: storageClassName,
+		VolumeName:       pvc.Spec.VolumeName,
+		VolumeMode:       volumeMode,
+		Reason:           "",
+		Capacity:         capacity,
+		CreationTime:     pvc.CreationTimestamp,
+		Content:          jsonBytes,
 	}
 	if err := d.storeDB.StorePVC(storeObj); err != nil {
-        log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
+		log.Log.Println("failed to store obj  ", storeObj, " err: ", err)
+		return err
+	}
+	return nil
+}
+
+func (d *ObjectStore) storeSubscription(sub *v1alpha1.Subscription) error {
+	jsonBytes, err := json.Marshal(sub)
+	if err != nil {
+		log.Log.Println("failed to marshal subscription object ", sub, " err: ", err)
+	}
+
+	storeObj := &Subscription{
+		UUID: string(sub.UID),
+
+		Name:            sub.Spec.Package,
+		Namespace:       sub.Namespace,
+		Source:          sub.Spec.CatalogSource,
+		SourceNamespace: sub.Spec.CatalogSourceNamespace,
+		StartingCSV:     sub.Spec.StartingCSV,
+		CurrentCSV:      sub.Status.CurrentCSV,
+		InstalledCSV:    sub.Status.InstalledCSV,
+		State:           string(sub.Status.State),
+
+		CreationTime: sub.CreationTimestamp,
+		Content:      jsonBytes,
+	}
+	if err := d.storeDB.StoreSubscription(storeObj); err != nil {
+		log.Log.Println("failed to store subscription obj  ", storeObj, " err: ", err)
 		return err
 	}
 	return nil
 }
 
 func (d *ObjectStore) processObject(obj interface{}) {
-    
+
 	switch obj.(type) {
 	case *k8sv1.Pod:
 		podObj := obj.(*k8sv1.Pod)
 		if err := d.storePod(podObj); err == nil {
-            log.Log.Println("stored obj  ", podObj)
+			log.Log.Println("stored obj  ", podObj)
 		}
 	case *k8sv1.Node:
 		nodeObj := obj.(*k8sv1.Node)
 		if err := d.storeNode(nodeObj); err == nil {
-            log.Log.Println("stored obj  ", nodeObj)
+			log.Log.Println("stored obj  ", nodeObj)
 		}
 	case *kubevirtv1.VirtualMachineInstance:
 		vmi := obj.(*kubevirtv1.VirtualMachineInstance)
 		if err := d.storeVmi(vmi); err == nil {
-            log.Log.Println("stored vmi obj  ", vmi)
-        }
+			log.Log.Println("stored vmi obj  ", vmi)
+		}
 	case *kubevirtv1.VirtualMachineInstanceMigration:
 		vmim := obj.(*kubevirtv1.VirtualMachineInstanceMigration)
-        vmimCopy := vmim.DeepCopy()
-        
+		vmimCopy := vmim.DeepCopy()
+
 		err := d.storeVmiMigration(*vmimCopy)
-        if err == nil {
-            log.Log.Println("stored vmi migration obj  ", vmimCopy)
-        } else {
-            log.Log.Println("failed to store vmi migration obj  ", vmimCopy)
-            
-        }
+		if err == nil {
+			log.Log.Println("stored vmi migration obj  ", vmimCopy)
+		} else {
+			log.Log.Println("failed to store vmi migration obj  ", vmimCopy)
+
+		}
 	case *k8sv1.PersistentVolumeClaim:
-       
+
 		pvc := obj.(*k8sv1.PersistentVolumeClaim)
 		if err := d.storePVC(pvc); err == nil {
-            log.Log.Println("stored pvc obj  ", pvc)
-        }
+			log.Log.Println("stored pvc obj  ", pvc)
+		}
+	case *v1alpha1.Subscription:
+		sub := obj.(*v1alpha1.Subscription)
+		if err := d.storeSubscription(sub); err == nil {
+			log.Log.Println("stored subscription obj  ", sub)
+		}
 	default:
 		jsonBytes, err := json.Marshal(obj)
 		if err != nil {
-            log.Log.Println("failed to marshal obj", obj, " err: ", err)
+			log.Log.Println("failed to marshal obj", obj, " err: ", err)
 		}
-        log.Log.Println("failed to process unknown object ", jsonBytes)
+		log.Log.Println("failed to process unknown object ", jsonBytes)
 	}
 }
 
@@ -370,25 +402,29 @@ func (d *ObjectStore) Add(obj interface{}) {
 	switch v := obj.(type) {
 	case *k8sv1.Pod:
 		podObj := obj.(*k8sv1.Pod)
-        d.wg.Add(1)    
-	    d.Queue.Add(podObj)
+		d.wg.Add(1)
+		d.Queue.Add(podObj)
 	case *k8sv1.Node:
 		nodeObj := obj.(*k8sv1.Node)
-        d.wg.Add(1)    
-	    d.Queue.Add(nodeObj)
+		d.wg.Add(1)
+		d.Queue.Add(nodeObj)
 	case *kubevirtv1.VirtualMachineInstance:
 		vmi := obj.(*kubevirtv1.VirtualMachineInstance)
-        d.wg.Add(1)    
-	    d.Queue.Add(vmi)
+		d.wg.Add(1)
+		d.Queue.Add(vmi)
 	case kubevirtv1.VirtualMachineInstanceMigration:
 		vmim := obj.(kubevirtv1.VirtualMachineInstanceMigration)
-        d.wg.Add(1)    
-	    d.Queue.Add(&vmim)
+		d.wg.Add(1)
+		d.Queue.Add(&vmim)
 	case *k8sv1.PersistentVolumeClaim:
 		pvc := obj.(*k8sv1.PersistentVolumeClaim)
-        d.wg.Add(1)    
-	    d.Queue.Add(pvc)
+		d.wg.Add(1)
+		d.Queue.Add(pvc)
+	case *v1alpha1.Subscription:
+		sub := obj.(*v1alpha1.Subscription)
+		d.wg.Add(1)
+		d.Queue.Add(sub)
 	default:
 		log.Log.Println("Cannot store unsupported obj ", v)
-    }
+	}
 }
