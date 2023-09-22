6628ea98207b4a80c93b60ae2c0be2780f45f22d
Vladik Romanovsky <vromanso@redhat.com>
2023-09-21 20:32:52 -0400

    add dsl query to collect logs from all vmi related componets
    Signed-off-by: Vladik Romanovsky <vromanso@redhat.com>

--- pkg/backend/dslQueryBuilder.go
+++ pkg/backend/dslQueryBuilder.go
@@ -44,6 +44,37 @@ func formatSingleVMIDSLQuery(res db.QueryResults) string {
     return vmiLogsQuery
 }
 
+func formatFullVMIHistoryDSLQuery(res db.QueryResults) string {
+
+    queryTemplate := `_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'%s',to:now))&_a=(columns:!(msg,podName,component,uid,subcomponent,reason,enrichment_data.pod.uid,enrichment_data.host.name,level),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:msg,negate:!f,type:exists,value:exists),query:(exists:(field:msg))),('$state':(store:appState),meta:(alias:!n,disabled:!f,key:msg,negate:!t,params:(query:'certificate with common name !'kubevirt.io:system:client:virt-handler!' retrieved.'),type:phrase),query:(match_phrase:(msg:'certificate with common name !'kubevirt.io:system:client:virt-handler!' retrieved.')))),interval:auto,query:(language:kuery,query:'containerName: "virt-controller" or containerName: "virt-api"%s%s%s or %s'),sort:!(!('@timestamp',asc)))`
+
+
+    startTimeStamp := res.StartTimestamp.Add(-time.Second * 30)
+    timeStamp := fmt.Sprintf("%sZ", startTimeStamp.UTC().Format("2006-01-02T15:04:05.000"))
+    podNames := ""
+    podUUIDs := ""
+
+    // handle involved virt-launchers
+    for _, pod := range res.InvolvedVirtLaunchers {
+        podNames += fmt.Sprintf(` or podName: "%s"`, pod.Name)
+        podUUIDs += fmt.Sprintf(` or "%s"`, pod.UUID)
+    }
+
+    // handle involved virt-handlers
+    for _, pod := range res.InvolvedVirtHandlers {
+        podNames += fmt.Sprintf(` or podName: "%s"`, pod.Name)
+        podUUIDs += fmt.Sprintf(` or "%s"`, pod.UUID)
+    }
+
+    // handle PVCs
+    pvcsList := handlePVCs(res)
+    
+    vmiLogsQuery := fmt.Sprintf(queryTemplate, timeStamp, podNames, podUUIDs, pvcsList, res.VMIUUID)
+
+    return vmiLogsQuery
+}
+
+
 func formatSinglePodDSLQuery(res db.QueryResults) string {
 
     queryTemplate := `_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:'%s',to:now))&_a=(columns:!(msg,podName,component,uid,subcomponent,reason,enrichment_data.pod.uid,enrichment_data.host.name,level),filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,key:msg,negate:!f,type:exists,value:exists),query:(exists:(field:msg))),('$state':(store:appState),meta:(alias:!n,disabled:!f,key:msg,negate:!t,params:(query:'certificate with common name !'kubevirt.io:system:client:virt-handler!' retrieved.'),type:phrase),query:(match_phrase:(msg:'certificate with common name !'kubevirt.io:system:client:virt-handler!' retrieved.')))),interval:auto,query:(language:kuery,query:'podName: "%s" or "%s"%s'),sort:!(!('@timestamp',asc)))`
